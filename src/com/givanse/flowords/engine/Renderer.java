46516888f768acfbc4611136f24cf59c34d6c077
givanse <silvag777@gmail.com>
2013-11-13 13:45:32 -0600

    drafting and commments

--- src/com/givanse/flowords/engine/Renderer.java
+++ src/com/givanse/flowords/engine/Renderer.java
@@ -63,15 +63,13 @@ public final class Renderer implements GLSurfaceView.Renderer {
 	// Animated offset time value for iterating between src and dst.
 	private long mOffsetTime;
 	
-	// Shader for rendering background gradient.
-	private final HelperShader mShaderBackground = new HelperShader();
-	// Shader for copying offscreen texture on screen.
-	private final HelperShader mShaderCopy = new HelperShader();
-	// Flag for indicating whether shader compiler is supported.
-	private final boolean[] mShaderCompilerSupported = new boolean[1];
+	/* Shaders */
+	private final HelperShader shdrBckndGradient = new HelperShader();
+	                                    /* Copies offscreen texture on screen */
+	private final HelperShader shdrCopyOffscreen = new HelperShader();
+	private final boolean[] isShaderCompilerSupported = new boolean[1];
 		
-	// Surface/screen dimensions.
-	private int mWidth, mHeight;
+	private int mWidth, mHeight;                 /* Surface/Screen dimensions */
 	
 	/**
 	 * Default constructor.
@@ -98,7 +96,7 @@ public final class Renderer implements GLSurfaceView.Renderer {
 	@Override
 	public synchronized void onDrawFrame(GL10 unused) {
 		// If shader compiler is not supported, clear screen buffer only.
-		if (mShaderCompilerSupported[0] == false) {
+		if (isShaderCompilerSupported[0] == false) {
 			GLES20.glClearColor(0, 0, 0, 1);
 			GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 			return;
@@ -132,12 +130,12 @@ public final class Renderer implements GLSurfaceView.Renderer {
 		this.helperFBO.bindTexture(0);
 
 		// Render background gradient.
-		this.mShaderBackground.useProgram();
-		int uAspectRatio = this.mShaderBackground.getHandleID("uAspectRatio");
-		int uOffset = this.mShaderBackground.getHandleID("uOffset");
-		int uLineWidth = this.mShaderBackground.getHandleID("uLineWidth");
-		int aPosition = this.mShaderBackground.getHandleID("aPosition");
-		int aColor = this.mShaderBackground.getHandleID("aColor");
+		this.shdrBckndGradient.useProgram();
+		int uAspectRatio = this.shdrBckndGradient.getHandleID("uAspectRatio");
+		int uOffset = this.shdrBckndGradient.getHandleID("uOffset");
+		int uLineWidth = this.shdrBckndGradient.getHandleID("uLineWidth");
+		int aPosition = this.shdrBckndGradient.getHandleID("aPosition");
+		int aColor = this.shdrBckndGradient.getHandleID("aColor");
 
 		float aspectX = (float) Math.min(mWidth, mHeight) / mHeight;
 		float aspectY = (float) Math.min(mWidth, mHeight) / mWidth;
@@ -163,8 +161,8 @@ public final class Renderer implements GLSurfaceView.Renderer {
 		// Copy FBO to screen buffer.
 		GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
 		GLES20.glViewport(0, 0, mWidth, mHeight);
-		this.mShaderCopy.useProgram();
-		aPosition = this.mShaderCopy.getHandleID("aPosition");
+		this.shdrCopyOffscreen.useProgram();
+		aPosition = this.shdrCopyOffscreen.getHandleID("aPosition");
 		GLES20.glVertexAttribPointer(aPosition, 2, GLES20.GL_BYTE, false, 0,
 				buffScreenVertices);
 		GLES20.glEnableVertexAttribArray(aPosition);
@@ -177,7 +175,7 @@ public final class Renderer implements GLSurfaceView.Renderer {
 	@Override
 	public void onSurfaceChanged(GL10 unused, int width, int height) {
 		// If shader compiler is not supported set viewport size only.
-		if (mShaderCompilerSupported[0] == false) {
+		if (isShaderCompilerSupported[0] == false) {
 			GLES20.glViewport(0, 0, width, height);
 			return;
 		}
@@ -193,10 +191,10 @@ public final class Renderer implements GLSurfaceView.Renderer {
 	public void onSurfaceCreated(GL10 unused, EGLConfig config) {
 		// Check if shader compiler is supported.
 		GLES20.glGetBooleanv(GLES20.GL_SHADER_COMPILER,
-				             mShaderCompilerSupported, 0);
+				             isShaderCompilerSupported, 0);
 
 		// If not, show user an error message and return immediately.
-		if (mShaderCompilerSupported[0] == false) {
+		if (isShaderCompilerSupported[0] == false) {
 			Handler handler = new Handler(mContext.getMainLooper());
 			handler.post(new Runnable() {
 					@Override
@@ -210,10 +208,10 @@ public final class Renderer implements GLSurfaceView.Renderer {
 			return;
 		}
 
-		this.mShaderCopy.setProgram(
+		this.shdrCopyOffscreen.setProgram(
 				                   mContext.getString(R.string.shader_copy_vs),
 				                   mContext.getString(R.string.shader_copy_fs));
-		this.mShaderBackground.setProgram(
+		this.shdrBckndGradient.setProgram(
 				             mContext.getString(R.string.shader_background_vs),
 				             mContext.getString(R.string.shader_background_fs));
 		mFlowerObjects.onSurfaceCreated(mContext);
